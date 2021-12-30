d52516b3461cc3f08593d3156ffc17dc25510368
PHUONG_DONG <dongphuong.maicogroup@gmail.com>
2021-12-30 15:48:12 +0700

    Add project files.

---
+++ TestOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
